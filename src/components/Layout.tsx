14c333dbea7383150b0aed9ea7fdaf77335d3d2a
robin jacobsson <robin23jac@gmail.com>
2025-04-27 14:51:03 +0200

    Use flex col for layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -30,7 +30,7 @@ const Layout = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen flex flex-col bg-gray-50">
       {/* Header */}
       <header className="bg-white shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
