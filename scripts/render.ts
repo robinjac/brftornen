b64c06bbcc17480db2ec296990372ab948d684d4
robin jacobsson <robin23jac@gmail.com>
2025-06-01 19:21:03 +0200

    Change to html output and remove whitespaces

--- scripts/render.ts
+++ scripts/render.ts
@@ -1,24 +1,45 @@
 import fs from "node:fs";
+import path from "node:path";
+
+async function clearPagesFolder(folderPath: string) {
+  try {
+    if (fs.existsSync(folderPath)) {
+      const files = fs.readdirSync(folderPath);
+      for (const file of files) {
+        fs.unlinkSync(path.join(folderPath, file));
+      }
+      console.log("Pages folder cleared.");
+    } else {
+      fs.mkdirSync(folderPath);
+      console.log("Pages folder created.");
+    }
+  } catch (error) {
+    console.error("Failed to clear pages folder:", error);
+  }
+}
 
 async function fetchWordPressPages() {
   try {
-    const response = await fetch("https://brftornen.se/wp-json/wp/v2/pages?per_page=100");
+    const response = await fetch(
+      "https://brftornen.se/wp-json/wp/v2/pages?per_page=100"
+    );
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
     const pages = await response.json();
 
+    const pagesFolder = path.resolve(__dirname, "../src/pages");
+    await clearPagesFolder(pagesFolder);
+
     // Example: print each page title
     pages.forEach((page) => {
       fs.writeFile(
-        "src/pages/" + page.slug + ".tsx",
-        `
-        <div className="prose">
+        "src/pages/" + page.slug + ".html",
+        `<div className="prose">
           <h1>${page.title.rendered.replace(/\//g, " / ")}<h1/>
-          <article>${page.content.rendered}</article>
-        </div>
-      `,
+          <article>${String(page.content.rendered)}</article>
+        </div>`.replace(/>\s+</g, "><"),
         (err) => {
           if (err) {
             console.error(`Error writing file for page ${page.slug}:`, err);
