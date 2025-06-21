a4cfb3d2361682ee00dfbb7bc039a2957e30cf3f
robin jacobsson <robin23jac@gmail.com>
2025-06-21 13:51:55 +0200

    Use static json

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,39 +1,28 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { Bell } from "lucide-react";
+import posts from "../../test/posts-response.json";
 
-const News = () => {
-  const [pages, setPages] = useState<WordpressPost[]>([]);
-
-  useEffect(() => {
-    fetch(`https://www.brftornen.se/wp-json/wp/v2/posts`)
-      .then((res) => res.json())
-      .then(setPages);
-  }, []);
-
-  if (pages.length === 0) return <p>Loading...</p>;
-
-  return (
-    <>
-      <div className="flex items-center space-x-2 mb-6">
-        <Bell className="h-6 w-6 text-blue-600" />
-        <h1 className="text-3xl !m-0 font-bold">NYHETER</h1>
-      </div>
-        {pages.map((item) => (
-          <article key={item.id} className="px-8 py-6 space-y-6 border-t">
-              <div>
-                <h2 className="text-xl font-semibold">{item.title.rendered}</h2>
-                <p className="text-gray-500 text-sm">
-                  {new Date(item.date).toLocaleDateString()}
-                </p>
-              </div>
-            <div
-              className="prose max-w-none"
-              dangerouslySetInnerHTML={{ __html: item.content.rendered }}
-            />
-          </article>
-        ))}
-    </>
-  );
-};
+const News = () => (
+  <>
+    <div className="flex items-center space-x-2 mb-6">
+      <Bell className="h-6 w-6 text-blue-600" />
+      <h1 className="text-3xl !m-0 font-bold">NYHETER</h1>
+    </div>
+    {posts.map((item) => (
+      <article key={item.id} className="px-8 py-6 space-y-6 border-t">
+        <header>
+          <h2 className="text-xl font-semibold">{item.title.rendered}</h2>
+          <p className="text-gray-500 text-sm">
+            {new Date(item.date).toLocaleDateString()}
+          </p>
+        </header>
+        <section
+          className="prose max-w-none"
+          dangerouslySetInnerHTML={{ __html: item.content.rendered }}
+        />
+      </article>
+    ))}
+  </>
+);
 
 export default News;
