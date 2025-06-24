7eaed3c741310d66f4be5288d573d92c2a845f76
robin jacobsson <robin23jac@gmail.com>
2025-06-24 13:33:05 +0200

    Use correct i18n url

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "i18n": path.resolve(__dirname, "./src/i18n"),
+      "i18n": path.resolve(__dirname, "./i18n"),
     },
   },
   base: "./"
