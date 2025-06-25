a5425aa2baa4ed974e6337289dab48e10ac8b2cd
robin jacobsson <robin23jac@gmail.com>
2025-06-25 22:29:54 +0200

    Add tilt to news icon

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -5,7 +5,7 @@ const News = () => (
   <>
     <div className="flex prose items-center mb-6">
       <h1 className="!m-0">NYHETER</h1>
-      <Bell className="ml-2 size-6 text-blue-600" />
+      <Bell className="ml-2 size-8 -rotate-12 text-blue-800" />
     </div>
     <Posts />
   </>
