be4c72c9f1ffc4c1789fea0326cc6850d025dece
robin jacobsson <robin23jac@gmail.com>
2025-06-25 09:22:57 +0200

    Adjust size of news title

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -3,9 +3,9 @@ import Posts from "@/components/Posts";
 
 const News = () => (
   <>
-    <div className="flex items-center space-x-2 mb-6">
-      <Bell className="h-6 w-6 text-blue-600" />
-      <h1 className="text-3xl !m-0 font-bold">NYHETER</h1>
+    <div className="flex prose -ml-2 items-center space-x-2 mb-6">
+      <Bell className="h-8 w-8 text-blue-600" />
+      <h1 className="!m-0">NYHETER</h1>
     </div>
     <Posts />
   </>
